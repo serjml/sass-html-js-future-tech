94423932f38e63bf768e066b5ae94768b4047867
Serj Lyakhov <serjml@list.ru>
2025-05-05 19:42:15 +0300

    feat: implement updateUI method & others

--- scripts/Select.js
+++ scripts/Select.js
@@ -1,6 +1,9 @@
+import BaseComponent from './BaseComponent.js';
+import MatchMedia from './MatchMedia.js';
+
 const rootSelector = '[data-js-select]';
 
-class Select {
+class Select extends BaseComponent {
   selectors = {
     root: rootSelector,
     originalControl: '[data-js-select-original-control]',
@@ -27,20 +30,98 @@ class Select {
     isExpanded: false,
     currentOptionIndex: null,
     selectedOptionElement: null,
-
-    constructor(rootElement) {
-      this.rootElement = rootElement;
-      this.originalControlElement = this.rootElement.querySelector(this.selectors.originalControl);
-      this.buttonElement = this.rootElement.querySelector(this.selectors.button);
-      this.dropdownElement = this.rootElement.querySelector(this.selectors.dropdown);
-      this.optionElements = this.dropdownElement.querySelectorAll(this.selectors.option);
-      this.state = {
-        ...this.initialStatae,
-        currentOptionIndex: this.originalControlElement.selectedIndex,
-        selectedOptionElement: this.optionElements[this.originalControlElement.selectedIndex],
-      };
-    },
   };
+
+  constructor(rootElement) {
+    super();
+    this.rootElement = rootElement;
+    this.originalControlElement = this.rootElement.querySelector(this.selectors.originalControl);
+    this.buttonElement = this.rootElement.querySelector(this.selectors.button);
+    this.dropdownElement = this.rootElement.querySelector(this.selectors.dropdown);
+    this.optionElements = this.dropdownElement.querySelectorAll(this.selectors.option);
+    this.state = this.getProxyState({
+      ...this.initialStatae,
+      currentOptionIndex: this.originalControlElement.selectedIndex,
+      selectedOptionElement: this.optionElements[this.originalControlElement.selectedIndex],
+    });
+    this.fixDropdownPosition();
+    this.updateTabIndexes();
+    this.bindEvents();
+  }
+
+  updateUI() {
+    const { isExpanded, currentOptionIndex, selectedOptionElement } = this.state;
+
+    const newSelectedOptionValue = selectedOptionElement.textContent.trim();
+
+    const updateOriginalControl = () => {
+      this.originalControlElement.value = newSelectedOptionValue;
+    };
+
+    const updateButton = () => {
+      this.buttonElement.textContent = newSelectedOptionValue;
+      this.buttonElement.classList.toggle(this.stateClassses.isExpanded, isExpanded);
+      this.buttonElement.setAttribute(this.stateAttributes.ariaExpanded, isExpanded);
+      this.buttonElement.setAttribute(
+        this.stateAttributes.ariaActiveDescendant,
+        this.optionElements[currentOptionIndex].id,
+      );
+    };
+
+    const updateDropdown = () => {
+      this.dropdownElement.classList.toggle(this.stateClassses.isExpanded, isExpanded);
+    };
+
+    const updateOptions = () => {
+      this.optionElements.forEach((optionElement, index) => {
+        const isCurrent = currentOptionIndex === index;
+        const isSelected = selectedOptionElement === optionElement;
+
+        optionElement.classList.toggle(this.stateClassses.isCurrent, isCurrent);
+        optionElement.classList.toggle(this.stateClassses.isSelected, isSelected);
+        optionElement.setAttribute(this.stateAttributes.ariaSelected, isSelected);
+      });
+    };
+
+    updateOriginalControl();
+    updateButton();
+    updateDropdown();
+    updateOptions();
+  }
+
+  fixDropdownPosition() {
+    const viewportWidth = document.documentElement.clientWidth;
+    const halfViewportWidth = viewportWidth / 2;
+
+    const { width, x } = this.buttonElement.getBoundingClientRect();
+    console.log(width);
+    console.log(x);
+    const buttonCenterX = x + width / 2;
+    const isButtonOnTheLeftViewportSide = buttonCenterX < halfViewportWidth;
+
+    this.dropdownElement.classList.toggle(
+      this.stateClassses.isOnTheLeftSide,
+      isButtonOnTheLeftViewportSide,
+    );
+
+    this.dropdownElement.classList.toggle(
+      this.stateClassses.isOnTheRightSide,
+      !isButtonOnTheLeftViewportSide,
+    );
+  }
+
+  updateTabIndexes(isMobileDevice = MatchMedia.mobile.matches) {
+    this.originalControlElement.tabIndex = isMobileDevice ? 0 : -1;
+    this.buttonElement.tabIndex = isMobileDevice ? -1 : 0;
+  }
+
+  onMobileMatchMediaChange = (event) => {
+    this.updateTabIndexes(event.matches);
+  };
+
+  bindEvents() {
+    MatchMedia.mobile.addEventListener('change', this.onMobileMatchMediaChange);
+  }
 }
 
 class SelectCollection {
